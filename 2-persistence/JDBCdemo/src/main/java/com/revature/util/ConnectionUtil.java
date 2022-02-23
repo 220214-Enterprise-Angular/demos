65c2bc3a63bfaa9bf73f7c2fdac5a6524be7056d
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-02-23 08:28:05 -0500

    add JDBC notes to ConnectionUtil.java

--- 2-persistence/JDBCdemo/src/main/java/com/revature/util/ConnectionUtil.java
+++ 2-persistence/JDBCdemo/src/main/java/com/revature/util/ConnectionUtil.java
@@ -11,24 +11,39 @@ import java.util.Properties;
 import org.apache.log4j.Logger;
 
 /**
- * Singleton Design Pattern:
- * - purpose: control object creation, limiting the number of instances of this class to 1
- *
+ * What is JDBC?
+ * 
+ * JDBC (Java Database Connectivity) is the Java API that manages 
+ * connecting to a database, issuing queries and commands, 
+ * and handling result sets obtained from the database
+ * 
+ * What is a Singleton Design Pattern? 
+ * 
+ * A Singleton Class is a software design pattern that ensures 
+ * there will be one single instance of that class.
+ * 
+ * -- private constructors 
+ * -- static field of an instance of the class to be returned 
+ * -- leverage a public static getInstance() (our getInstance method is the getConnection() method
+ * 
+ * The connection to our DB will be established by a singleton class which will
+ * contain all the necessary driver information (jdbc url, username, password).
  */
 public class ConnectionUtil {
 	
 	private static Logger logger = Logger.getLogger(ConnectionUtil.class);
 	
-	// import the Connection interface from java.sql
+	// import the Connection interface from java.sql (JDBC API!)
 	private static Connection conn = null;
 	
-	// singlton's have private constructors
+	// Singleton's have private constructors
 	private ConnectionUtil() {
 		
 	}
 	
-	// every singlton class has a public static getInstance() method
-	public static Connection getConnection() {
+	// every Singleton class has a public static getInstance() method
+	// This allows us to return the single instance that exists in the application
+	public static Connection getConnection() { 
 		
 		try {
 			// first we check is an instance exists
@@ -43,28 +58,24 @@ public class ConnectionUtil {
 			return null;
 		}
 		
-		// Here we actually establish a connection to the database by passing the appropriate credentials to  
-		// connect to it (username & password + JDBC url)
-		
+		// We use a Properties object to read from a file and scrape our credentials from it
 		Properties prop = new Properties(); // ctrl + shift + o will auto import from java.util
 			
 		String url = "";
 		String username = "";
 		String password = "";
 		
-		
 		try {
-			
 			// The Properties object uses the FileReader object to read all the values from the document that we've specified at the path below
 			prop.load(new FileReader("C:\\Users\\SophieGavrila\\Desktop\\demos\\2-persistence\\JDBCdemo\\src\\main\\resources\\application.properties")); // import FileReader from java.io
 			
 			url = prop.getProperty("url"); // we are able to assign the string value of the value of the "url" field in the application.properties file
-			username = prop.getProperty("username");
+			username = prop.getProperty("username"); // These all correlate the the "key" of the key-value pairs in the application.properties file
 			password = prop.getProperty("password");
 			
 			conn = DriverManager.getConnection(url, username, password); 
-			// IF the above line is successful we WON'T hit any of these catch clauses and the code will execute line 80 (return conn)
-			logger.info("Successfully connected to DB");
+			// IF the above line is successful we WON'T hit any of these catch clauses and the code will execute line 93 (return conn)
+			logger.info("Successfully connected to DB"); // If we've hit this line, we've successfully established a connection with the DB
 			
 		} catch (SQLException e) {
 			logger.error("Can't establish connection to DB with given credentials");
@@ -73,7 +84,7 @@ public class ConnectionUtil {
 			logger.warn("couldn't read from application.properties at specified path");
 			e.printStackTrace();
 		} catch (IOException e) {
-			logger.warn("Something wrong with application.props file");
+			logger.warn("Something wrong with application.properties file");
 			e.printStackTrace();
 		}
 		
