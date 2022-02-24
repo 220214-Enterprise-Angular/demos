7b9054289665c92d0f09d49376aca5af85396664
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-02-24 11:37:16 -0500

    add  full joins statement

--- 2-persistence/JDBCdemo/src/main/resources/setup.sql
+++ 2-persistence/JDBCdemo/src/main/resources/setup.sql
@@ -28,10 +28,7 @@ INSERT INTO sophiag.users (username, pwd, user_role_name)
 			('Marry', '1234', 'Admin'),
 			('Barry', 'pass', 'Employee');
 		
-SELECT * FROM sophiag.users;
-SELECT * FROM sophiag.accounts;
-SELECT * FROM sophiag.users_accounts_jt;
-		
+
 DROP TABLE IF EXISTS sophiag.users_accounts_jt CASCADE;
 CREATE TABLE users_accounts_jt (
 	
@@ -44,6 +41,66 @@ CREATE TABLE users_accounts_jt (
 -- the info in the users_accounts_jt table is automatically updated
 -- triggers & functions
 
+CREATE OR REPLACE FUNCTION sophiag.auto_insert_into_jt()
+RETURNS TRIGGER 
+AS $$
+
+	BEGIN
+		-- INSERT INTO ANOTHER TABLE
+		INSERT INTO sophiag.users_accounts_jt(acc_owner, account)
+		-- INSERTING THE VALUES OF NEW.ACC_OWNER, NEW.ID <- THAT'S POINTING TO THE ID OF THE ACCOUNT THAT THE USER OWNS
+			VALUES (NEW.acc_owner, NEW.id);
+		
+		RETURN NEW; -- MAKE SURE WE STILL CONTINUE TO INSERT THE ACCOUNT RECORD INTO THE ACCOUNTS TABLE
+	END
+
+$$ 
+LANGUAGE plpgsql;
+
+CREATE TRIGGER trig 
+	AFTER INSERT ON sophiag.accounts 
+	FOR EACH ROW
+	EXECUTE FUNCTION sophiag.auto_insert_into_jt();
+
+
+SELECT * FROM sophiag.users;
+SELECT * FROM sophiag.accounts;
+SELECT * FROM sophiag.users_accounts_jt;
+		
+INSERT INTO sophiag.accounts (balance, acc_owner, active) 
+	VALUES (2000, 1, TRUE);
+
+INSERT INTO sophiag.accounts (balance, acc_owner, active) 
+	VALUES (20, 2, FALSE),
+			(1000000, 4, TRUE);
+		
+		
+-- THIS IS GOING TO BE A join statement so we can return all user data and the user's associated accounts
+		
+		
+-- user id, username, pwd, role, account id, account balance, isActive
+SELECT users.id, users.username, users.pwd, users.user_role_name, accounts.id AS account_id, accounts.balance, accounts.active
+	FROM users
+	LEFT JOIN users_accounts_jt ON users.id = users_accounts_jt.acc_owner 
+	LEFT JOIN accounts ON accounts.id  = users_accounts_jt.account;
+		
+		
+		
+		
+
+-- this is only returning the accounts for one person
+SELECT sophiag.accounts.id, sophiag.accounts.balance 
+FROM sophiag.accounts
+	INNER JOIN sophiag.users_accounts_jt 
+		ON sophiag.accounts.id = sophiag.users_accounts_jt.acc_owner 
+			WHERE sophiag.users_accounts_jt.acc_owner = 2;
+		
+		
+		
+		
+		
+		
+		
 
 
 
