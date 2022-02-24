da9f4aad0040897a73a2650f6b8b1264229ad9e0
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-02-23 21:44:02 -0500

    add chinook joins to joins.sql

--- 2-persistence/scripts/joins.sql
+++ 2-persistence/scripts/joins.sql
@@ -1,15 +1,40 @@
--- Kinds of Joins
+SELECT * FROM "Artist";
+SELECT * FROM "Album";
 
--- Left JOIN
--- Right Join
--- Inner Join
--- Outer Join 
+-- Inner join
+-- left join
+-- right join
+-- outter join (full join)
 
-SELECT * FROM posts;
-SELECT * FROM users;
+-- return the album ID, track title, and artist name by joining both THE
+-- album and artists TABLES 
+CREATE VIEW simple_album_artist_view AS
+SELECT alb."AlbumId", alb."Title", art."Name" AS artist
+	FROM "Album" alb
+	JOIN "Artist" art 
+	ON alb."ArtistId" = art."ArtistId";
 
-CREATE VIEW most_posts AS SELECT users.id, first_name, last_name, count(*) AS count_num FROM users
-	LEFT JOIN posts ON posts.author_id = users.id
-	GROUP BY users.id;
+SELECT * FROM simple_album_artist_view;
 
-SELECT * FROM most_posts;
+
+-- the above could also be expressed as a natural join 
+SELECT * FROM "Artist" 
+	NATURAL JOIN "Album";
+
+-- SELF JOIN
+SELECT * FROM "Employee";
+
+SELECT e1."FirstName", e1."Title", e2."FirstName" AS boss_first_name, e2."Title" AS boss_title
+	FROM "Employee" e1
+	JOIN "Employee" e2
+	ON e1."ReportsTo" = e2."EmployeeId";
+
+SELECT * FROM chinook."Track";
+SELECT * FROM chinook."InvoiceLine";
+
+-- trackID, track name, invoicelineID, Invoice Id ON trackID 
+SELECT a."TrackId", a."Name", b."InvoiceLineId", b."InvoiceId"
+	FROM chinook."Track" AS a
+	LEFT JOIN chinook."InvoiceLine" AS b 
+	ON a."TrackId" = b."TrackId";
+
