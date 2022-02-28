c777cbf791bc59c1c2c76e5ea66cd8dc07523941
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-02-28 14:36:32 -0500

    add lambdas

---
+++ 3-javaSE-apis/AdvancedJava/src/main/java/com/revature/lambdas/Driver.java
@@ -0,0 +1,107 @@
+package com.revature.lambdas;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		Employee abby = new Employee("Abby Adams", 40);
+		Employee bob = new Employee("Bob Briggs", 22);
+		Employee carter = new Employee("Carter Cobb", 20);
+		Employee doug = new Employee("Doug Doberman", 52);
+		Employee emily = new Employee("Emily Evanson", 34);
+		
+		// instantiate an ArrayList
+		List<Employee> employees = new ArrayList<>();
+		
+		employees.add(emily);
+		employees.add(carter);
+		employees.add(abby);
+		employees.add(doug);
+		employees.add(bob);
+		
+		/**
+		 * A Lambda expression is a short block of code which takes in parameters and returns a value.
+		 * Lambda Expressions are similar to methods but they don't have an name
+		 */
+		employees.forEach(e -> System.out.println(e));
+		
+		// the old way with an enhanced for-loop
+		for (Employee e : employees) {
+			System.out.println(e);
+		}
+		
+		// multiple lines in our lambda functionality?
+		employees.forEach(e -> {
+			// mutliple lines are now allowed (notice the curly brackets)
+			System.out.println("Name is "+ e.getName());
+			System.out.println("Age is "+ e.getAge());
+			System.out.println();
+		});
+		
+		// forEach can take in an instance of a class that has implemented the Consumer Interface
+		// forEach can take in an Anonymous Class that implements the consumer Interface
+		// forEach can simply use a lambda described the implementation of the accept() method that comes from the Consumer Functional Interface	
+	
+//		Collections.sort(employees); // we haven't implemented the Comparable interface and overriden the compareTo() method!
+	
+		// External class that implements Comparator
+		Collections.sort(employees, new NameSorter());
+		
+		// Anonymous Class:
+		Collections.sort(employees, new Comparator<Employee>() {
+
+			@Override
+			public int compare(Employee e1, Employee e2) { // instantiating an anonymous class that implements Comparator
+				// TODO Auto-generated method stub
+				return e1.getName().compareTo(e2.getName()); 
+			} 
+		});
+		
+		// Lambda
+		Collections.sort(employees, (e1, e2) -> e1.getName().compareTo(e2.getName()));
+		
+		
+		/**
+		 * Find a way to sort the employees list by Age.
+		 * 
+		 * 1. Sort by creating a Comparator external class.
+		 * 2. Sort by providing an Anonymous class.
+		 * 3. Sort with a Lambda in 1 line.
+		 * 
+		 * 
+		 */
+		
+		
+		
+		
+	}	
+}
+
+// create the comparator class
+class NameSorter implements Comparator<Employee> {
+
+	@Override
+	public int compare(Employee e1, Employee e2) {
+		// TODO Auto-generated method stub
+		return e1.getName().compareTo(e2.getName()); // we can compare two strings becasue the String class has overridden the compareTo
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 3-javaSE-apis/AdvancedJava/src/main/java/com/revature/lambdas/Employee.java
@@ -0,0 +1,35 @@
+package com.revature.lambdas;
+
+public class Employee {
+
+	private String name;
+	private int age;
+
+	public Employee(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", age=" + age + "]";
+	}
+
+}
