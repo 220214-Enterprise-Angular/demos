3e2da83e5547e7c05b32a0ff391829e927ec4320
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-03-04 12:50:13 -0500

    add the meta model print out demo to the ormDemo driver

--- 3-javaSE-apis/HelloHibernate/src/main/java/com/revature/OrmDemo.java
+++ 3-javaSE-apis/HelloHibernate/src/main/java/com/revature/OrmDemo.java
@@ -12,51 +12,45 @@ import com.revature.util.MetaModel;
 import com.revature.util.PrimaryKeyField;
 
 public class OrmDemo {
-	
+
 	public static void main(String[] args) {
-		
-		
-		// create list of classes to pass to constructor 
+
+		// create list of classes to pass to constructor
 		List<Class<?>> myClasses = Arrays.asList(DummyUser.class, DummyOtherUser.class);
-		
-		
+
 		// build a configuration object from the ReflectionFramework
-		
-		
-		
+
 		// import from the com.revature.ReflectioFramework
 		Configuration cfg = new Configuration();
-		
-		// Drawback of reflection is that it's an expensive runtime operation to inspect all the class at runtime
-		cfg.addAnnotatedClasses(myClasses);
-		
-		System.out.println(cfg.getMetaModels()); // generate a toString() fo rhte metaModel class
-		
-		// iterate over each metaModel within the configruation class
-		for (MetaModel<?> metaModel : cfg.getMetaModels()) {
-			
-			// for each one, print the information about the metaModel
-			System.out.println("Printing all the MetModel info for the class %s\n" + metaModel.getClassName());
-			PrimaryKeyField pk = metaModel.getPrimaryKey();
-			List<ColumnField> columns = metaModel.getColumns();
-//			List<ForeignKeyField> foreignKeys = metaModel.getForeignKeys();
-			
-			for (ColumnField cf : columns) {
-				System.out.println("Found the column field named " + cf.getColumnName() + " with the type " + cf.getType());
-			}
-			
-			
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
 
+		// Drawback of reflection is that it's an expensive runtime operation to inspect
+		// all the class at runtime
+		cfg.addAnnotatedClasses(myClasses);
+
+		System.out.println(cfg.getMetaModels()); // generate a toString() fo rhte metaModel class
+
+		for (MetaModel<?> metamodel : cfg.getMetaModels()) {
+
+			System.out.printf("Printing MetaModel for class: %s\n", metamodel.getClassName());
+			PrimaryKeyField pk = metamodel.getPrimaryKey();
+			List<ColumnField> columns = metamodel.getColumns();
+			List<ForeignKeyField> foreignKeyFields = metamodel.getForeignKeys();
+
+			System.out.printf(
+					"\t Found a primary key field named %s, of type %s, which maps to the column with name: %s\n",
+					pk.getName(), pk.getType(), pk.getColumnName());
+
+			for (ColumnField column : columns) {
+				System.out.printf(
+						"\t Found a column field named %s, of type %s, which maps to the column with name: %s\n",
+						column.getName(), column.getType(), column.getColumnName());
+			}
+
+			for (ForeignKeyField foreignKey : foreignKeyFields) {
+				System.out.printf(
+						"\t Found a foreign key field named %s, of type %s, which maps to the column with name: %s\n",
+						foreignKey.getName(), foreignKey.getType(), foreignKey.getColumnName());
+			}
+		}
+	}
 }
