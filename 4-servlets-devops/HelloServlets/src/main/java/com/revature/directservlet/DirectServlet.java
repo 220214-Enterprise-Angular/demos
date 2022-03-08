e805ed64f67021eb5471dc2d364200898798ae92
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-03-08 11:10:58 -0500

    add forward() and sendRedirect()

--- 4-servlets-devops/HelloServlets/src/main/java/com/revature/directservlet/DirectServlet.java
+++ 4-servlets-devops/HelloServlets/src/main/java/com/revature/directservlet/DirectServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.models.SuperVillain;
+
 /**
  * This class is a Servlet class.
  * This class extends server side capabilities of a java program.
@@ -17,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class DirectServlet extends HttpServlet { // this comes from the Servlet API 
 
-	// we need a method to handle a GET request
+	// we need a method to handle a GET request at /dirserv
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
 		
@@ -34,8 +37,42 @@ public class DirectServlet extends HttpServlet { // this comes from the Servlet 
 		// with a GET request
 		
 		// We need to map this servlet class to a specific end point http://localhost:8080/HelloServlets/dirserv
-		
+		// By setting up the servlet mapping in the web.xml file
 	}
+	
+	
+	// Here I'm giving this servlet the ability to handle a post request
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		
+		// ideally a JSON object would be posted aka sent to this servlet and maybe we could transform it to a Java
+		// object, then store it in a database.
+		// question becomes: how do we transform a JSON object into a java object of a class that we have defined?
+		// we need an object mapper 
+		
+		// Step 1. Set response type as JSON
+		response.setContentType("application/json");
+		
+		// Step 2. Instantiate a SuperVillain Object
+		SuperVillain vill = new SuperVillain("The Penguin", "money", 9000.10); // ctrl + shift + o
+		
+		// Step 3. Transform the Java villain object to JSON AND write it to the client with the print writer
+		PrintWriter out = response.getWriter(); // grabbing the object that allows me to send data to a client
+		
+		// transofmr the villain object from java to a JSON string 
+		// we are instantiating the Object Mapper object from the Jackson Object Mapping library
+		String jsonString = new ObjectMapper().writeValueAsString(vill); // returns a JSON string
+		
+		// grab our print writer whose job is to write info to be displayed in the browser
+		out.write(jsonString); 
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
---
+++ 4-servlets-devops/HelloServlets/src/main/java/com/revature/indirectservlet/IndirectServlet.java
@@ -0,0 +1,58 @@
+package com.revature.indirectservlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndirectServlet extends HttpServlet {
+	
+	
+    // IF this servlet recieves a GET request, re-direct it to our DirectServlet at /dirserv
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.sendRedirect("http://localhost:8080/HelloServlets/dirserv");
+		// When you use sendRedirect, a new request is created and the URL is rewritten
+//		response.sendRedirect("https://google.com");
+			
+		System.out.println("Triggered the doGet() method within IndirectServlet and redirected to DirectServlet");		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// another way of facilitating inter-servlet communication: forward() method from the RequestDispatcher
+		RequestDispatcher rdis = request.getRequestDispatcher("/dirserv");
+		
+		rdis.forward(request, response); // the DirectServlet becomes responsible for handling the
+										 // forwarded request and delivering a response.
+		System.out.println("doPost() method triggered in IndirectServlet");
+	}
+
+	/*
+	 * Here is a list of major differences between servlet forward and
+	 * sendRedirect()
+	 * 
+	 * sendRedirect():
+	 * 
+	 * The request is redirected to a different resource The client will see the URL
+	 * change after the redirect A totally new request is created Redirect is
+	 * normally used within Post/Redirect/Get web development pattern
+	 *
+	 * 
+	 * forward():
+	 * 
+	 * The request will be further processed on the server side The client isn't
+	 * impacted by forward, URL in a browser stays the same Request and response
+	 * objects will remain the same object after forwarding. Request-scope objects
+	 * will be still available
+	 */
+	
+	
+	
+	
+	
+}
