e8fa6f339bbafe21bf2df64dd1eaac3a59b169f4
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-03-22 12:47:12 -0400

    add the beginning to student table

--- 6-html-css-js/JS/6-array.js
+++ 6-html-css-js/JS/6-array.js
@@ -2,19 +2,19 @@ let scores = new Array(10); // instantiating an Array object by passing a length
 
 console.log(scores);
 //              0       1       2
-let colors = ['red', 'blue', 'green'];
+let colors = ["red", "blue", "green"];
 console.log(colors);
 
 // accessing elements within the array
-console.log(colors[1]) // return the second element ('blue')
+console.log(colors[1]); // return the second element ('blue')
 // reassign the value of an element at the 2 index (3rd element\)
-colors[2] = 'purple';
+colors[2] = "purple";
 console.log(colors);
 
 let size = colors.length; // grabs the # of elements in the array;
 
 // JS arrays are dyanmic - we can change the size as we pleaseand add/remove element
-colors.push('yellow');
+colors.push("yellow");
 console.log(colors);
 
 // remove an element
@@ -26,7 +26,7 @@ colors.shift(); // removes the first element ('red')
 console.log(colors);
 
 // capture the index
-console.log(colors.indexOf('purple')) // returns the index of purple
+console.log(colors.indexOf("purple")); // returns the index of purple
 // returns -1 if it doesn't exist;
 
 // declare some variables
@@ -35,43 +35,54 @@ let b = 10;
 let c = true;
 let d = {};
 let e = null;
-let f = '';
+let f = "";
 let g = 0;
 let h = [];
-let i = function(){} // assigning the variable i to an anonymous function
+let i = function () {}; // assigning the variable i to an anonymous function
 
 // build an array called list and add all the variables to it
 const list = [a, b, c, d, e, f, g, h, i]; // arrays in JS can hold multiple different types
 
 // iterate over it in 3 different ways
-for (let j=0; j<list.length; j++) { //          == BASIC FOR LOOP ===
+for (let j = 0; j < list.length; j++) {
+  //          == BASIC FOR LOOP ===
 
-    console.log(list[j]) // print the element at the index of j
+  console.log(list[j]); // print the element at the index of j
 }
 
-console.log('------------------------------------------------------')
+console.log("------------------------------------------------------");
 
 //                                              == FOR IN LOOP ===
 // returns the index of each element in the list
 for (let elIndex in list) {
-    console.log(list[elIndex])
+  console.log(list[elIndex]);
 }
 
-console.log('------------------------------------------------------')
+console.log("------------------------------------------------------");
 
 //                                              == FOR OF LOOP ==
 // returns the actual element ( no need for the index) - similar to enhanced for-loop
 for (let el of list) {
-    console.log(el);
+  console.log(el);
 }
 
-console.log('------------------------------------------------------')
+console.log("------------------------------------------------------");
 
 // forEach() method
-list.forEach(x => {
-    console.log(x)
-})
+list.forEach((x) => {
+  console.log(x);
+});
 
-list.forEach(x => console.log(x))
+list.forEach((x) => console.log(x));
 
-// callback functions
+// callback functions - functions that can be passed as parameters
+
+// it's assuming that the function we're passing as a parameter takes in 1 parameter itself
+list.forEach(doSomething); // notice that there are NO () after the method
+
+// I will define a function to be passed as a callback
+// this will do what I want to do TO EACH ELEMENT IN THE ARRAY that I call it on
+
+function doSomething(input) { // input represents the element it's operating on
+  console.log(`The input is ${input}`);
+}
---
+++ 6-html-css-js/JS/student-table/app.js
@@ -0,0 +1,54 @@
+console.log('app.js is working!')
+
+// Goal: When we clkick the button take the input and add it below
+
+// Capture the button, save it to a variable, add an Event Listener
+let button = document.getElementById('add-student');  // querySelector('button') // returns the first button
+button.addEventListener("click", addStudent); // 2 params: event to listen for, and what to do when it happens (callback function)
+
+// when it's clicked, do some function
+
+
+// define the function to add all values from the input fields to the table
+function addStudent() {
+
+    // 1. Grab the input element AND its field values from the student name & major
+    let nameField = document.getElementById('student-name').value; // returns whatever the user entered
+    let majorField = document.getElementById('student-major').value;
+
+    // 2.check that they're truthy
+    if (nameField && majorField) { // if () statement automatically checks for truthy/falsey so no need for !!
+
+        // 3. if the are, create a row element
+        let row = document.createElement('tr'); // 'tr' means <tr>
+        
+        // 4. Create a a table cell element for ID
+        let idCell = document.createElement('td'); // represents individual cells in a table
+
+        // 5. Create a a table cell element for Name
+        let nameCell = document.createElement('td');
+
+        // 6. Create a a table cell element for Major
+        let majorCell = document.createElement('td');
+
+        // 7. Append the cells to the newly created row
+        row.appendChild(idCell);
+        row.appendChild(nameCell);
+        row.appendChild(majorCell);
+
+        // 8. capture the pre-existing table and append the row to it
+        document.getElementById('student-table-body').appendChild(row);
+
+        // 9. set the inner text of each cell the information we captured from the input
+        idCell.innerText = 1; // add a function that returns a value that auot-increments
+        nameCell.innerText = nameField;
+        majorCell.innerText = majorField;
+
+        // set the input fields to '' every time we click the button
+
+    }
+}
+
+// clear out the input field
+
+// we need some function to increment the ID
